6e94c2c44cff3b29329556f25653f189c117c80a
arakis2 <51448379+arakis2@users.noreply.github.com>
2023-05-03 17:38:39 +0200

    amélioration choix image

--- src/components/PokemonImageCard.vue
+++ src/components/PokemonImageCard.vue
@@ -16,11 +16,11 @@ export default {
 
 </script>
 <template>
-  <div className="col s10 m6 l4">
+  <div className="col s12 m4 l4">
     <div className="card hoverable">
       <div className="card">
         <div className="card-image">
-          <img :src="url" :alt="title" style="width:225px; margin: 0 auto">
+          <img className="responsive-img" :src="url" :alt="title">
         </div>
       </div>
     </div>
